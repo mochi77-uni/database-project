dd71f81d6495d76a172e388a40f736bb8e06354a
jimmy-lee0831 <jimmy890831@gmail.com>
2022-06-15 17:46:03 +0800

    sql

--- html/sign.php
+++ html/sign.php
@@ -24,8 +24,7 @@ if (isset($_POST['account']) && isset($_POST['password'])) {
 	$password = $_POST['password'];
 
 	$sql_q1 = "select password from user where ID = $account;";	// ******** update your personal settings ******** 
-	$result = mysqli_query($conn,$sql_q1);
-<<<<<<< HEAD:php/sign.php
+	$result = mysqli_query($conn,$sql_q1); 
 	if($result->num_rows > 0) {
         while($row = $result1->fetch_assoc()) {
 			if($row[password] == $password ){
@@ -44,24 +43,7 @@ if (isset($_POST['account']) && isset($_POST['password'])) {
 	else{
 		echo "連結失敗";
 	}
-		
-	
-=======
-	if ($result == $password) {
-        header("Location: main.php");
-        exit;
-	} else if($result == NULL) {
-		echo  "<script type='text/javascript'>alert('無此帳號!!');</script>";
-	} else {
-		echo  "<script type='text/javascript'>alert('密碼錯誤!!');</script>";
-	}
-	// } else if($result == NULL) {
-	// 	echo "<h2 align='center'><font color='antiquewith'>無此帳號!!</font></h2>";
-	// } else {
-	// 	echo "<h2 align='center'><font color='antiquewith'>密碼錯誤!! </font></h2>";
-	// }
 
->>>>>>> 9df9e0604b8e3ddbec86f22ec9f9f00e6acd521e:html/sign.php
 }else{
 	echo "資料不完全";
 	echo $account;
