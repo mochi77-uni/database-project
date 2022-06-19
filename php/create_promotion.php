7235ce4b96751ef3cbf082628b863528c150edc0
jimmy-lee0831 <jimmy890831@gmail.com>
2022-06-19 20:29:50 +0800

    sal

--- php/create_promotion.php
+++ php/create_promotion.php
@@ -19,14 +19,23 @@ if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 } 
 
-if (isset($_POST['ID']) && isset($_POST['start']) && isset($_POST['end']) && isset($_POST['content'])) {
-    $ID = $_POST['ID'];
+if (isset($_POST['start']) && isset($_POST['end']) && isset($_POST['content'])) {
     $start = $_POST['start'];
     $end = $_POST['end'];
     $content = $_POST['content'];
-
+    $sql_q0 = "select ID from promotion where ID not in (select A.ID from promotion as A, promotion as B where  A.ID<B.ID );";
     $sql_q1 = "insert into promotion valus ( '$ID', '$start', '$end', '$content' );";
-	$result1 = mysqli_query($conn,$sql_q1);
+	$result0 = mysqli_query($conn,$sql_q0);
+    while($row = $result1->fetch_row()) {
+        $ID = $row[0];
+    }
+    $num = (int)$ID;
+    $num = $num +１;
+    $ID = (string)$num;
+    $ID = str_pad($ID,5,"0",STR_PAD_LEFT);
+
+
+    $result1 = mysqli_query($conn,$sql_q1);
     if($result1){
         echo "新增成功<br>";
     }
