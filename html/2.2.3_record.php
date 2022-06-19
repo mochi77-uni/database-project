60ec14732363f12c7e5c49e69482b4b480bddf39
jimmy-lee0831 <jimmy890831@gmail.com>
2022-06-20 03:06:30 +0800

    sql

--- html/2.2.3_record.php
+++ html/2.2.3_record.php
@@ -31,12 +31,11 @@ $trans_ID = $_SESSION['trans_ID'];
 echo('<div align="center" >');
 if ($trans_ID != 0) {
 
-	$trans_ID = $_POST['trans_ID'];
 	$now_time = date("Y-m-d H:i:s");
 	$sql_q1 = "update transaction set invalid_time = '$now_time' where ID = '$trans_ID';";
 	$sql_q2 = "delete from books_trans where transaction_ID = '$trans_ID';";
-	$result1 = mysqli_query($conn,$sql_q1);
 	$result2 = mysqli_query($conn,$sql_q2);
+	$result1 = mysqli_query($conn,$sql_q1);
     if($result1 && $result2){
 		echo("<div>成功刪除 ".$trans_ID.$now_time);
 	}
