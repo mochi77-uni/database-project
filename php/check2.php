cc6b64a9ffa51c98a827ea9252aa4f3f17654872
jimmy-lee0831 <jimmy890831@gmail.com>
2022-06-20 00:30:56 +0800

    sql

--- php/check2.php
+++ php/check2.php
@@ -19,54 +19,24 @@ if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 } 
 
-if (isset($_POST['way']) && isset($_POST['user_ID']) && isset($_POST['time']) && isset($_POST['total'])  && isset($_POST['book_ID']) && isset($_POST['amount'])) {
-	$trans_ID;
-	$temp = 0;
-	$way = $_POST['way'];
-	$user_ID = $_POST['user_ID'];
-	$time = $_POST['time'];
-	$total = $_POST['total'];
-	$book_ID = $_POST['book_ID'];
-	$amount = $_POST['amount'];
-	
-	$sql_q0 = "select max((int)ID) from transaction";//目前trans的最後一號
-	$trans_ID = mysqli_query($conn,$sql_q0);
-	$temp = (int)$trans_ID;
-	$temp = $temp+1;
-	$trans_ID = strval($temp);
-	$trans_ID = str_pad($trans_ID, 5, "0", STR_PAD_LEFT);//new ID
-
-	$sql_q1 = "select content from promotion;";//應放在check.html
-	$sql_q2 = "insert into transaction values('$trans_ID', '$way', '$user_ID', '$time', '$total', NULL);";
-	$sql_q3 = "insert into books_trans values('$book_ID', '$trans_ID', '$amount');";
-	$result1 = mysqli_query($conn,$sql_q1); 
-	$result2 = mysqli_query($conn,$sql_q2);
-	$result3 = mysqli_query($conn,$sql_q3);
-
-	if($result1->num_rows > 0) {//應放在check.html
-        while($row = $result1->fetch_assoc()) {
-			printf("%s <br>", $row["content"]);
-			exit;        
-        }
-    }
-    else {
-    printf('無法查看promotion<br>');
-    }
-
-	if($result2 && $result3){
-		echo "交易成功<br>";
+if (1) {
+	$total = 0;
+	$product[] = $_POST['product'];
+	$amount[] = $_POST['amount'];
+	$j = count($product);
+	for($i=0 ; $i<$j ; $i++){
+		$sql_q1 = "select price from transaction where ID = product[$i];";
+		$result1 = mysqli_query($conn,$sql_q1);
+		if($result1->num_rows > 0) {//應放在check.html
+			while($row = $result1->fetch_row()) {
+				$total += $row[0]*$amount[$i];
+			}
+		}
 	}
-	else{
-		echo "交易失敗<br>";
-	}
-
-
 }else{
 	echo "資料不完全";
 }
 mysqli_free_result($result1);
-mysqli_free_result($result2);
-mysqli_free_result($result3);
 $conn→close();		
 ?>
 
---
+++ php/check3.php
@@ -0,0 +1,81 @@
+<?php
+
+// ******** update your personal settings ******** 
+$servername = "localhost";
+$username = "root";
+$password = "db_project";
+$dbname = "db_project";
+
+// Connecting to and selecting a MySQL database
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+if (!$conn->set_charset("utf8")) {
+    printf("Error loading character set utf8: %s\n", $conn->error);
+    exit();
+}
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+} 
+
+if (isset($_POST['way']) && isset($_POST['time']) && isset($_POST['total'])) {
+	$trans_ID;
+	$temp = 0;
+	$way = $_POST['way'];
+	$user_ID = $_POST['user_ID'];
+	$time = $_POST['time'];
+	$total = $_POST['total'];
+	$book_ID = $_POST['book_ID'];
+	$amount = $_POST['amount'];
+	
+	$sql_q0 = "select ID from transaction where ID not in (select A.ID from transaction as A, transaction as B where  A.ID<B.ID );";//目前trans的最後一號
+	$result0 = mysqli_query($conn,$sql_q0);
+	if($result1){
+        if($result1->num_rows > 0){
+			while($row = $result1->fetch_row()) {
+				$trans_ID = $row[0];
+			}
+		}
+	}
+	
+	$temp = (int)$trans_ID;
+	$temp = $temp+1;
+	$trans_ID = strval($temp);
+	$trans_ID = str_pad($trans_ID, 5, "0", STR_PAD_LEFT);//new ID
+
+	$sql_q1 = "select content from promotion;";//應放在check.html
+	$sql_q2 = "insert into transaction values('$trans_ID', '$way', '$user_ID', '$time', '$total', NULL);";
+	$sql_q3 = "insert into books_trans values('$book_ID', '$trans_ID', '$amount');";
+	$result1 = mysqli_query($conn,$sql_q1); 
+	$result2 = mysqli_query($conn,$sql_q2);
+	$result3 = mysqli_query($conn,$sql_q3);
+
+	if($result1->num_rows > 0) {//應放在check.html
+        while($row = $result1->fetch_assoc()) {
+			printf("%s <br>", $row["content"]);
+			exit;        
+        }
+    }
+    else {
+    printf('無法查看promotion<br>');
+    }
+
+	if($result2 && $result3){
+		echo "交易成功<br>";
+	}
+	else{
+		echo "交易失敗<br>";
+	}
+
+
+}else{
+	echo "資料不完全";
+}
+mysqli_free_result($result0);
+mysqli_free_result($result1);
+mysqli_free_result($result2);
+mysqli_free_result($result3);
+$conn→close();		
+?>
+
